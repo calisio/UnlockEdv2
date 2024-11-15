decdfcc90fab78730866983072bf5128318f9b1d
calisio <calisio@wustl.edu>
2024-11-15 15:42:24 -0600

    fix:update oc dashboard card

--- frontend/src/Components/cards/OpenContentCard.tsx
+++ frontend/src/Components/cards/OpenContentCard.tsx
@@ -7,13 +7,13 @@ export default function OpenContentCardRow({
 }) {
     return (
         <div className="card flex flex-row w-full gap-3 px-4 py-2">
-            <div className="w-[75px]">
+            <div className="w-[100px]">
                 <img
-                    className="h-12 mx-auto"
+                    className="h-12 mx-auto object-contain"
                     src={content.thumbnail_url ?? ''}
                 ></img>
             </div>
-            <h3 className="my-auto">{content.name ?? 'Untitled'}</h3>
+            <h3 className="my-auto w-full">{content.name ?? 'Untitled'}</h3>
         </div>
     );
 }
